d3221e9d80d628bc922dda737e0deacf328fa99d
unknown <dessa.neves99@gmail.com>
2024-12-16 11:42:28 -0300

    Exceções

---
+++ exceptions_ts/Conversor.ts
@@ -0,0 +1,15 @@
+﻿try{
+const resultado = converte('abc')
+
+console.log(resultado)
+}catch(error: any){
+    if(error instanceof TypeError)
+        console.error("Voce digitou um valor que não é do tipo string!")
+    else
+    console.error("Erro Insperado!", error.message)
+} finally{
+    console.log("Programa Finalizado!")
+}
+export function converte(conteudo: any): string {
+    return conteudo.toUpperCase()
+}
